94c5e9be42cbbd4756a54b2d08f95d1c437d3fec
HGiorgis <hailegiorgiswagaye@gmail.com>
2025-11-18 15:38:52 +0300

    Api Landinge page for better info

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
-Route::get('/debug-users', function () {
-    $users = DB::table('users')->get();
-    
-    echo "<h1>The Debug is working!!!</h1>";
-    foreach ($users as $user) {
-        echo "Email: " . $user->email . "<br>";
-        echo "Hash: " . $user->auth_key_hash . "<br>";
-        echo "Hash Length: " . strlen($user->auth_key_hash) . "<br><br>";
-    }
-    
+Route::get('/', function () {
+    return view('welcome');
 });
